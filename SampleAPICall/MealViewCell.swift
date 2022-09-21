80d3963aec92b291af5f364d1737305e70ce6fe5
Sibusiso94 <58597982+Sibusiso94@users.noreply.github.com>
2022-09-21 08:12:41 +0200

    Create Meal Cell

---
+++ SampleAPICall/MealViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealViewCell.swift
+//  SampleAPICall
+//
+//  Created by Sibusiso Mbonani on 2022/09/21.
+//
+
+import UIKit
+
+class MealViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SampleAPICall/ViewController.swift
+++ SampleAPICall/ViewController.swift
@@ -24,7 +24,5 @@ class ViewController: UIViewController {
             }
         }
     }
-
-
 }
 
