5feec96ab25a70e167ab3be233f0407b99a76761
Sibusiso94 <58597982+Sibusiso94@users.noreply.github.com>
2022-09-21 08:49:49 +0200

    Edit Message Cell

--- SampleAPICall/MealViewCell.swift
+++ SampleAPICall/MealViewCell.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class MealViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var mealView: UIView!
+    
+    @IBOutlet weak var mealImageView: UIImageView!
+    
+    @IBOutlet weak var mealTitleLabel: UILabel!
+    
+    @IBOutlet weak var mealCategoryLabel: UILabel!
+    
+    @IBOutlet weak var mealCaloryLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        mealView.layer.cornerRadius = mealView.frame.size.height / 15
+        
+        mealImageView.layer.cornerRadius = mealImageView.frame.size.height / 2
+        mealImageView.clipsToBounds = true
+        mealImageView.layer.borderWidth = 3
+        mealImageView.layer.borderColor = UIColor.white.cgColor
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- SampleAPICall/ViewController.swift
+++ SampleAPICall/ViewController.swift
@@ -8,21 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     
     let dp = DataProvider()
-//    let api = ApiManager()
+    var foodHints = [Hint]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         
 //        api.performRequest()
         dp.parseJSON { [self] in
             
             if let foodArray = dp.foods {
-                print(foodArray.hints)
-
+                foodHints = foodArray.hints
             }
         }
+        
+        tableView.register(UINib(nibName: "MealViewCell", bundle: nil), forCellReuseIdentifier: "mealCell")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mealCell", for: indexPath) as! MealViewCell
+        
+        return cell
     }
 }
 
